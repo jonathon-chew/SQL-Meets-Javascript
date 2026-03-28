163601599347ecd294fcb0df9b3394cdb91b2c4d
Jonathon Chew <jonchew626@hotmail.com>
2026-03-28 18:28:51 +0000

    new: assert to bubble up error messages to the user for common mistakes - like the from statement needing captials, missing information; update: insert into to work with the new functionality

--- script.js
+++ script.js
@@ -10,32 +10,78 @@ function RunSQLCommand() {
 
     if (SQLCommand.includes("CREATE TABLE")) {
         let table = SQLCommand.split("CREATE TABLE")[1].trim()
+        if (!assert(table, { gtlength: 1 })) {
+            showError("Could not find a table name after the command CREATE TABLE " + table.length)
+            return
+        }
         createTable(table)
         return 0
     }
 
     if (SQLCommand.includes("DROP TABLE")) {
         let table = SQLCommand.split("DROP TABLE")[1].trim()
+        if (!assert(table, { gtlength: 1 })) {
+            showError("Could not find a table name after the command DROP TABLE " + table.length)
+            return
+        }
         dropTable(table)
         return 0
     }
 
+    // If we're not doing an insert table / drop table check we have the basics
+    if (!assert(SQLCommand, { contains: "FROM" })) {
+        showError("No FROM statement")
+        return
+    }
+
+    if (!assert(SQLCommand, { contains: "SELECT" })) {
+        showError("No SELECT statement")
+        return
+    }
+
+    // Check the from statement has a table name after it
+    if (!assert(fromStatement[1].trim(), { type: "string", gtlength: 0 })) {
+        showError("No table name found after FROM statement")
+        return
+    }
+
     let tableName = fromStatement[1].trim().split(' ')[0];
-    let selectStatement = fromStatement[0].trim().split(' ').slice(1);
+    let selectStatement = fromStatement[0].trim().split('SELECT').slice(1);
     let tableID = document.getElementById(tableName).value
 
-    if (selectStatement[0] === '*') {
+    if (selectStatement[0].trim() === '*') {
         showEntireTable(tableID);
     } else { // Print the columns listed - split by comma
         let outputString = ''
+        console.log("selectStatement is ", selectStatement)
         outputString = getSelectStatement(selectStatement, tableID)
         showEntireTable(outputString)
     }
 
     if (SQLCommand.includes("INSERT INTO")) {
         let tableToInsertTo = SQLCommand.split("SELECT")[0].split("INSERT INTO")[1].trim()
-        document.getElementById(tableToInsertTo).value = getSelectStatement(selectStatement, tableID)
-    };
+
+        // CHECK THE TABLE TO INSERT INTO EXISTSs
+        let getTags = document.getElementsByTagName("textArea")
+        let checkTableExists = []
+        for (let id in getTags) {
+            if (getTags[id].id) {
+                checkTableExists.push(getTags[id].id)
+            }
+        }
+        if (!assert(checkTableExists, { contains: tableToInsertTo })) {
+            showError("No table exists yet with that name to insert into!")
+            return
+        }
+
+        console.log("selectStatement is ", selectStatement[0].trim())
+        if (selectStatement[0].trim() === '*') {
+            console.log("Matching the value of the tables insert:", tableToInsertTo, " from ", tableName)
+            document.getElementById(tableToInsertTo).value = document.getElementById(tableName).value
+        } else {
+            document.getElementById(tableToInsertTo).value = getSelectStatement(selectStatement, tableID)
+        };
+    }
 
 }
 
@@ -91,10 +137,10 @@ function getSelectStatement(selectStatement, tableFinder) {
         let columnTitle = firstLine.split(',')
 
         // Remove the comma - statement is split by space NOT by comma so remove it to make it match
-        statement = statement.replace(',', '');
+        statement = statement.replace(',', '').trim();
 
         // Find (if at all) where the statement column is
-        console.log(statement, columnTitle.indexOf(statement));
+        console.log("/", statement, "/", columnTitle.indexOf(statement));
 
         // Push to the columnsToUseArray the index of the statement to be used later
         columnsToUse.push(columnTitle.indexOf(statement));
@@ -120,7 +166,85 @@ function getSelectStatement(selectStatement, tableFinder) {
         outputString = outputString + "\n"
     }
 
+    // console.log(outputString)
     return outputString
+}
+
+function assert(variable, {
+    type = null,
+    gtlength = -Infinity,
+    ltlength = Infinity,
+    eqlength = null,
+    contains = "",
+    isNot = "",
+} = {}) {
+
+    if (type !== null) {
+        if (typeof (variable) !== type) {
+            console.error(variable.type + " is not type " + type)
+            return false
+        }
+    }
+
+    if (gtlength !== -Infinity) {
+        if (variable.length <= gtlength) {
+            console.error(variable.length + " is less than" + gtlength)
+            return false
+        }
+    }
+
+    if (ltlength !== +Infinity) {
+        if (variable.length >= ltlength) {
+            console.error(variable.length + " is greater than" + ltlength)
+            return false
+        }
+    }
+
+    if (eqlength !== null) {
+        if (variable.length !== eqlength) {
+            console.error(variable.length + " is equal to" + eqlength)
+            return false
+        }
+    }
+
+    let found = false
+    if (contains != "") {
+        if (typeof (variable) == "string") {
+            if (!variable.includes(contains)) {
+                console.error(variable + " is a string and does not contain " + contains)
+                return false
+            }
+        } else if (typeof (variable) == "object") {
+            console.log("Checking array: ", variable)
+            for (let item in variable) {
+                console.log("Item in the array to check: ", variable[item])
+                if (variable[item].includes(contains)) {
+                    found = true
+                    break
+                }
+            }
+            if (!found) {
+                console.error(variable + " is an array of length " + variable.length + " does not contain " + contains)
+                return false
+            }
+        }
+    }
+
+    if (variable == isNot) {
+        return false
+    }
+
+    return true
+
+}
+
+function showError(errorMessage) {
+
+    // Get the result table
+    const resultTable = document.getElementById("resultTable");
+
+    // Clear the existing table content + just make it the error message
+    resultTable.innerHTML = errorMessage;
 }
 
 function showEntireTable(textInput) {
